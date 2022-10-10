20a746fa288dcea64941eb1c881a4ca0e96d00e9
Alex Matos Iuasse <alexiuasse@gmail.com>
2022-10-10 16:47:53 -0300

    Fix database not being connected

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,6 +5,7 @@ then
     echo "Waiting for postgres..."
 
     while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
+      echo "Waiting for postgres... $DATABASE_HOST $DATABASE_PORT"
       sleep 0.1
     done
 
