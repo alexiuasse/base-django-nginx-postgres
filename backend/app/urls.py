c791ba84d93fb98f559415fdb931b81170136c1a
Alex Matos Iuasse <alexiuasse@gmail.com>
2022-10-10 16:10:10 -0300

    Add docker settings

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from upload.views import image_upload
 
 urlpatterns = [
-    path("", image_upload, name="upload"),
+    path('__debug__/', include('debug_toolbar.urls')),
     path("admin/", admin.site.urls),
 ]
 
--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,14 +4,16 @@ if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
+    while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
       sleep 0.1
     done
 
     echo "PostgreSQL started"
 fi
 
-python manage.py flush --no-input
+# python manage.py flush --no-input
+python manage.py collectstatic
+# python manage.py makemigrations
 python manage.py migrate
 
 exec "$@"
