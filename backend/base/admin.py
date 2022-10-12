bf2117cea8ba61113a827d5b164f30614002f740
Alex Matos Iuasse <alexiuasse@gmail.com>
2022-10-12 16:03:58 -0300

    Add soft delete admin

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -3,4 +3,17 @@ from django.contrib import admin
 from .models import FakeModelTest
 
 
-admin.site.register(FakeModelTest)
+class SoftDeletedModelAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        qs = self.model.deleted_objects.get_queryset()
+        ordering = self.get_ordering(request)
+        if ordering:
+            qs = qs.order_by(*ordering)
+        return qs
+
+
+class FakeModelTestAdmin(SoftDeletedModelAdmin):
+    pass
+
+
+admin.site.register(FakeModelTest, FakeModelTestAdmin)
